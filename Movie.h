8e4f0ef3bbd1163f838f343f5af20e9350d75bb3
Vanabren <vanabren@ut.utm.edu>
2022-11-02 16:55:40 -0500

    Movie.h doq

--- Movie.h
+++ Movie.h
@@ -2,14 +2,22 @@
  * @file Movie.h
  * @author Vance Brenderabrandis + Jacqueline Bybee
  * @date 2022-10-31
- * @brief later
+ * @brief Declaration of the Movie struct
  * 
- * later
+ * Declares the Movie struct which serves as nodes for our Library's collection stl::linked list
  */
 
 #ifndef MOVIE_H
 #define MOVIE_H
 
+
+/**
+ * Movie struct which holds all relevant info for movies in our collection
+ *
+ * @class Movie Movie.h "Movie-Collection/Movie.h"
+ * @brief Movie struct with relevant info for movies
+ *
+ */
 struct Movie {
   std::string title;
   std::string directorName;
